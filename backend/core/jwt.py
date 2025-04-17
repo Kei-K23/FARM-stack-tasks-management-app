e40c9766b0eff4e1f15694ec244f5f59220428e6
Kei-K23 <keiksl2301@gmail.com>
2025-04-17 09:48:27 +0630

    feat: setup jwt and password hashing functions

---
+++ backend/core/jwt.py
@@ -0,0 +1,23 @@
+import jwt
+from jwt.exceptions import InvalidTokenError
+from datetime import timedelta, datetime, timezone
+from core.config import settings
+
+
+def create_access_token(data: dict) -> str:
+    """Create Access Token
+
+    data: dist -> JWT Payload
+    """
+    to_encode = data.copy()  # Copy access token payload
+    expire = datetime.now(timezone.utc) + \
+        timedelta(minutes=settings.access_token_expire_minutes)
+    to_encode.update({"exp": expire})  # Add exp field to the payload
+    encoded_jwt = jwt.encode(
+        to_encode, settings.secret_key, algorithm=settings.algorithm)
+    return encoded_jwt
+
+
+def decode_token(token: str):
+    """Decode the token"""
+    return jwt.decode(jwt=token, key=settings.secret_key, algorithms=[settings.algorithm])
